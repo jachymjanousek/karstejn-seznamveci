fabc9229c8032336786baeb6fc916d17ab90f100
Jáchym Janoušek <jachym.janousek@woltair.cz>
2023-05-21 14:57:02 +0200

    MVP version

---
+++ src/lib/data.js
@@ -0,0 +1,182 @@
+export const data = {
+  sections: [
+    {
+      title: "Na cestu",
+      items: [
+        "batoh na výlet",
+        "pití",
+        "pokrývka hlavy",
+        "mikina nebo větrovka",
+        "svačina na cestu",
+      ],
+    },
+    {
+      title: "Hygiena",
+      items: [
+        "ručníky (ideálně 2 ks)",
+        "zubní kartáček a pasta",
+        "šampón",
+        "sprchový gel",
+        "toaletní papír",
+        "opalovací krém",
+        "papírové kapesníky",
+      ],
+      bonus: [
+        "hřeben",
+      ]
+    },
+    {
+      title: "Oblečení",
+      items: [
+        "14x spodní prádlo (kalhotky nebo trenýrky)",
+        "2x teplé ponožky a dostatečný počet těch normálních",
+        "6x triko s krátkým rukávem",
+        "2x triko s dlouhým rukávem",
+        "3x mikina",
+        "bunda (větrovka)",
+        "pláštěnka",
+        "2x kraťasy",
+        "4x tepláky, dlouhé kalhoty",
+        "plavky",
+        "oblečení na spaní",
+        "šátek",
+        "bílé triko na batikování / barvení",
+      ],
+    },
+    {
+      title: "Jídelní servis",
+      items: [
+        "ešus  (kompletní tj. 3 ks - víčko, malý ešus, velký ešus)",
+        "příbor: lžíce, vidlička, nůž",
+        "hrnek s uchem (nerozbitny, nejlépe nerezový nebo plastový)",
+      ],
+    },
+    {
+      title: "Obuv",
+      items: [
+        "2x Sportovní obuv",
+        "nepromokavé boty (holinky)",
+        "boty do vody (stačí gumové pantofle s páskem přes patu)",
+      ],
+      bonus: [
+        "sandále",
+      ]
+    },
+    {
+      title: "Na Spaní",
+      items: ["spacák ", "karimatka"],
+      bonus: [ "polštář", "deka" ]
+    },
+    {
+      title: "Ostatní",
+      items: [
+        "2x repelent (doporučujeme Predátor)",
+        "látkový vak na špinavé prádlo",
+        "sešit nebo blok na psaní",
+        "psací potřeby (pastelky, tužky, propisky)",
+        "Světlo – baterka nebo čelovka",
+        "náhradní baterie",
+      ],
+      bonus: [
+        "kapsář, rozhodně doporučujeme. 🙂 (viz Naše tipy)",
+        "kapesní nůž",
+        "dopisní papíry a obálky s poštovní známkou",
+        "hry (např. karty, pexeso, atd.)",
+      ]
+    },
+  ],
+};
+
+export const tips = {
+  items: [
+    {
+      title: "Kufr, taška nebo krosna ",
+      detail: [
+        "Doporučujeme <b>kufr nebo přenosnou tašku</b>. Umožňují přehledné uspořádání věcí než například krosny. Doprava zavazadel je zajištěna až na místo, takže se nemusíte bát, že by se děti někam vlekly se všemi věcmi.",
+        "Kufr se snadno uloží pod postel a nepřekáží.",
+      ],
+    },
+    {
+      title: "Spacák",
+      detail: [
+        "Nejdůležitější kus výbavy je kvalitní spacák.",
+        "Doma předem zkontrolujte, že je spacák v pořádku. Nejlepší je, když dítě do pytle vleze a vyzkouší, že jde celý zapnout a není moc velký.",
+      ],
+    },
+    {
+      title: "Batoh",
+      detail: [
+        "Batoh na cestu a výlety přichystejte velikostně přijatelný dítěti. Měla by se do něj vejít mikina, pláštěnka, láhev s pitím a svačina.",
+      ],
+    },
+    {
+      title: "Boty",
+      detail: [
+        "Nemusí to být nové, značkové boty. Důležité je, aby nikde netlačily. Většinu her hrajeme v lese a tam sandále neobstojí.",
+      ],
+    },
+    {
+      title: "Kapesné",
+      detail: [
+        "Kapesné dejte dětem dle svého vlastního uvážení, ale je zbytečné dávat dětem velké částky (doporučujeme <b>do 300 Kč</b>, víc není potřeba).",
+      ],
+    },
+    {
+      title: "Pláštěnka",
+      detail: [
+        "Pořiďte dítěti kvalitní pláštěnku z pevného PVC nebo pogumované tkaniny.<br/>Jednorázové pláštěnky z jemného igelitu obvykle nevydrží více použití.",
+      ],
+    },
+    {
+      title: "Cennosti",
+      detail: [
+        "Cennosti (např. zlaté náušnice) ve svém vlastním zájmu raději nedávejte.<br/>Mobilní telefony, mp3 přehrávače a jiné drahé předměty tomu podobné si děti berou na vlastní nebezpečí. <b>Neručíme za poškození nebo ztrátu.</b>",
+        "Mobil ani jiné elektronické přístroje nelze dobíjet.",
+        "Děti mají možnost si zavolat domů, pokud budou chtít. V případě potřeby vás budeme kontaktovat my.",
+      ],
+    },
+    {
+      title: "Oblečení",
+      detail: [
+        "Doporučujeme dávat dětem na tábor věci starší, vyzkoušené a zároveň kvalitní. Aktivity probíhají převážně v lesích nebo na louce, mimo „civilizaci“.",
+        "Zkontrolujte všechny velikosti oblečení, hlavně bot, zda sedí a nepatří mladším sourozencům.",
+      ],
+    },
+    {
+      title: "Léky",
+      detail: [
+        "Pokud vaše dítě pravidelně užívá nějaké léky, předejte je u autobusu zdravotnici. <br/>Žádné léky nesmí mít děti u sebe.",
+        "Nedávejte dětem s sebou jiné léky nepravidelně užívané (paralen, brufen, apod.), jsou v táborové lékárně – v případě potřeby je podá naše zdravotnice, abychom měli přehled. I my, instruktoři a vedoucí, si chodíme za naší zdravotnicí pro léky. 👩🏻‍⚕️",
+      ],
+    },
+    {
+      title: "Sladkosti",
+      detail: [
+        "Nedávejte dětem větší množství sladkostí, zamezíte tím bolesti břicha a nezvaným návštěvníkům ve stanu. Jsme uprostřed lesa, takže ježci nebo hlodavci mohou zavítat.",
+      ],
+    },
+    {
+      title: "Ešus",
+      detail: [
+        "Všechny kusy soupravy ešusů prosím označte. <b>Viditelně</b>, nesmývatelnou značkou. Zamezíte tím každoročním problémům jako:",
+        "<i>“Tohle víčko není moje .”</i>",
+        "<i>“Já jsem to neměl poškrábaný.”</i>",
+        "<i>“Mně to nepasuje k sobě.”</i>",
+        "<i>“Já si svůj poznám!”</i>",
+        "Neoznačené kusy, označíme sami.",
+      ],
+    },
+    {
+      title: "Kapsář",
+      detail: [
+        "V podsadových stanech je polička mezi postele. Menší předměty jako kartáček nebo pasta však rády zapadají z poličky za postel.<br/>Kapsář je možné pověsit na okraj podsady a mít tak drobné věci po ruce.",
+        "Návod na jeho vytvoření najdete <a href='https://www.youtube.com/watch?v=m6Uf5fZl9Fc'>zde</a>.",
+      ],
+    },
+  ],
+};
+
+export default {
+  sections: data.sections,
+  tips,
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,176 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {
+    AccordionItem,
+    Heading,
+    P,
+    A,
+    Label,
+    Alert,
+    Checkbox,
+    Card,
+  } from "flowbite-svelte";
+  import { ArrowDown, ArrowUp, Check } from "svelte-bootstrap-svg-icons";
+  import data from "$lib/data.js";
+
+  export const snapshot = {
+    capture: () => list,
+    restore: (value) => (list = value),
+  };
+
+  const listMemory = () =>
+    data.sections.reduce(
+      (acc, section) => ({
+        ...acc,
+        [section.title]: section.items.map(() => 0),
+        [section.title + "bonus"]: section.bonus?.map(() => 0),
+      }),
+      {}
+    );
+  let list = listMemory();
+</script>
+
+<svelte:head>
+  <title>Karštejn – Seznam věcí s sebou</title>
+</svelte:head>
+
+<main class="stack">
+  <Heading>Co s sebou na Karštejn?</Heading>
+  <div class="stack">
+    <P>
+      Máte strach z balení věcí? Vždy na něco zapomenete?<br />Projděte si náš
+      chytrý seznam.
+    </P>
+    <P>
+      Máte strach z balení věcí? Vždy na něco zapomenete?<br />Projděte si náš
+      chytrý seznam. Aby měly děti sebou všechno potřebné, připravili jsme
+      doporučený seznam věcí na dětský tábor
+    </P>
+    <P
+      >Jednotlivé položky si můžete odškrtnout. Až se sem vrátíte, seznam si
+      bude pamatovat, co už máte odškrtnuté.</P
+    >
+    <P>
+      Máte radši v ruce papír nebo chcete seznam přibalit dětem s sebou? Seznam
+      si můžete stahnout a vytisknout zde:
+      <A
+        href="https://docs.google.com/document/d/1ziAREXzcZgufkk7Pec6bIkuwzzUzDwYlzbf4Y1xWdIQ/edit?usp=sharing"
+        >Co s sebou</A
+      >
+    </P>
+    <!-- <P>
+      K položkám se symbolem <b>~</b> najdete více info v kateogrii
+      <b>Naše Tipy</b>
+    </P> -->
+  </div>
+
+  <Heading tag="h2">Seznam</Heading>
+  <div>
+    {#each data.sections as section, sectionIndex}
+      <AccordionItem id={sectionIndex}>
+        <svelte:fragment slot="header">
+          <Heading tag="h3">
+            {section.title}
+          </Heading>
+          <P class="mr-4">
+            {list[section.title].filter(Boolean).length}/{list[section.title]
+              .length}
+          </P>
+        </svelte:fragment>
+        <span slot="arrowup">
+          <ArrowUp />
+        </span>
+        <span slot="arrowdown">
+          <ArrowDown />
+        </span>
+        <div class="stack stack-4">
+          {#each section.items as item, itemIndex}
+            <Label>
+              <Checkbox bind:checked={list[section.title][itemIndex]} />
+              <span class="pl-2">{item}</span>
+            </Label>
+          {/each}
+          <Heading tag="h5">Nepovinné</Heading>
+          {#if section.bonus?.length}
+            {#each section.bonus as bonus, bonusIndex}
+              <Label>
+                <Checkbox
+                  bind:checked={list[section.title + "bonus"][bonusIndex]}
+                />
+                <span class="pl-2">{bonus}</span>
+              </Label>
+            {/each}
+          {/if}
+        </div>
+      </AccordionItem>
+    {/each}
+  </div>
+
+  <Heading tag="h2">Naše tipy</Heading>
+  <div>
+    <div class="cluster">
+      <div style="align-items: flex-start;">
+        {#each data.tips.items as tip, tipIndex}
+          <Card>
+            <Heading tag="h3">{tip.title}</Heading>
+            <div class="stack">
+              {#each tip.detail as p, itemIndex}
+                <P>{@html p}</P>
+              {/each}
+            </div>
+          </Card>
+        {/each}
+      </div>
+    </div>
+  </div>
+
+  <Heading tag="h2">U autobusu</Heading>
+  <div>
+    <P>K rychlému odbavení dětí při odjezdu, si prosím připravte:</P>
+    <ul class="list-disc">
+      <li>Originál přihlášky</li>
+      <li>Originál potvrzení od lékaře</li>
+      <li>
+        Potřebné léky, pokud je dítě po dobu tábora musí užívat společně s
+        informacemi kolik, jak často a kdy (2x denně, ráno, večer, před jídlem,
+        po jídle, ...)
+      </li>
+      <li>
+        Průkaz zdravotní pojišťovny (<b>stačí kopie</b>)
+      </li>
+      <!-- <li>
+      Čestné prohlášení o bezinfekčnosti. <b
+        >Originál s podpisem a s datem začátku tábora:
+      </b>
+      <br />I. běh (<A
+        href="https://docs.google.com/document/d/1bqhbdzMt6zAGex4PUUkVKN55W6OHB7EU3ViIvT-LDrM/edit?usp=sharing"
+        >odkaz</A
+      >)
+      <br />II. běh (<A
+        href="https://docs.google.com/document/d/1v6ecAEoKdgclalnzstZGMwh1lEYGuaIe0Xo-Q9-0fxs/edit?usp=sharing"
+        >odkaz</A
+      >)
+    </li> -->
+    </ul>
+  </div>
+  <Alert
+    >Něco jsme zapomněli? Nevíte si rady nebo jen se chcete doptat? Neváhejte
+    se na nás obrátit. Rádi vám pomůžeme.</Alert
+  >
+  <ul>
+    <li><A href="tel:+420604405795">+420 604 405 795</A> (Jáchym Janoušek)</li>
+    <li><A href="mailto:info(at)karstejn.org">info(at)karstejn.org</A> (Schránku kontrolujeme zpravidla 2x týdne)</li>
+    <li><A href="https://www.karstejn.org">www.karstejn.org</A></li>
+    <li><A href="https://www.facebook.com/LTKarstejn">facebook</A></li>
+    <li><A href="https://www.instagram.com/karstejn">instagram</A></li>
+  </ul>
+</main>
+
+<style lang="scss" global>
+  body {
+    padding: 3rem 1rem;
+  }
+  main {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+</style>
